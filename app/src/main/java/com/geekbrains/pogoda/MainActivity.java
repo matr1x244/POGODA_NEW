e638da253a046ad28b137a37e237c23d0f8a0925
matr1x244 <zio-21@yandex.ru>
2020-10-22 23:03:07 +0300

    Погода

---
+++ app/src/main/java/com/geekbrains/pogoda/MainActivity.java
@@ -0,0 +1,22 @@
+package com.geekbrains.pogoda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_main);
+
+        String hello_str = getResources().getString(R.string.app_name);
+        hello_str +="!!!!";
+
+
+    }
+}
