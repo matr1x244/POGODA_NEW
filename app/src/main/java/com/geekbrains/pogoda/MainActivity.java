85f5ff53160aebbcaf54b4e8e5d337a14acdb28f
matr1x244 <zio-21@yandex.ru>
2020-11-14 19:57:59 +0300

    CityFragment.java:92: выдает:
    CityFragment.java:92: error: incomparable types: String and int
    if (detailsFragment == null || currentCity.getCityName() != detailsFragment.getCurrentCity().getImageMonuments()) { //??
    указывая стрелкой ^ на ( !=)
    
    CityFragment.java:
    uses or overrides a deprecated API.
    Recompile with -Xlint:deprecation for details.
    
    CoatFragment.java:47 выдает:
    
    CoatFragment.java:47: error: method getResourceId in class TypedArray cannot be applied to given types;
    coatCity.setImageResource(imgs.getResourceId(city.getImageMonuments() -1)); // ??
    ^
    required: int,int
    found: int
    reason: actual and formal argument lists differ in length.
    
    --
    
    если закоментить эти пока что строчки приложение запускается но когда я выбираю город он открывает фрагмент нужный город - он его переносит что да это москва например но картинка всегда соответствует одной не меняется взависимости от города.

--- app/src/main/java/com/geekbrains/pogoda/MainActivity.java
+++ app/src/main/java/com/geekbrains/pogoda/MainActivity.java
@@ -80,7 +80,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
             Toast.makeText(this, "Поиск..", Toast.LENGTH_SHORT).show();
             Log.d(TAG, "buttonSearchSend");
             Intent search = new Intent(this, ActivityWeather.class);
-            search.putExtra("Город..", cityName.getText().toString()); // тут ошибка ( getText() )
+            search.putExtra("Город..", cityName.getQuery().toString());
             startActivity(search);
         }
     }
